d7988e53d7886daff20dde6910431bab6766afa4
Thomas Boyt <thomas@ledgerx.com>
2014-12-11 17:14:52 -0500

    Add BaseEntity fields, even if they aren't being used correctly

--- src/entities/BaseEntity.js
+++ src/entities/BaseEntity.js
@@ -6,8 +6,32 @@
 var Game = require('../Game');
 var Coquette = require('coquette');
 
+type Coordinates = {
+  x: number;
+  y: number;
+};
+
 class BaseEntity {
-  // Interface hooks
+  /*
+   * Public fields
+   * TODO: These aren't being checked properly yet, see
+   * https://github.com/thomasboyt/coquette-boilerplate/issues/3
+   */
+
+  center: Coordinates;
+  size: Coordinates;
+  angle: number;
+  zIndex: number;
+
+  // TODO: Should be some kinda enum w/ Coquette.Collider.RECTANGLE | Coquette.Collider.CIRCLE
+  // (at worst, this could be a number once an interface is declared for Coquette, since those
+  // two constants are just 0 and 1)
+  boundingBox: any;
+
+  /*
+   * Public interface
+   */
+
   constructor(game: Game, settings: Object): void {}
   draw(ctx: any): void {}
   update(dt: number): void {}
